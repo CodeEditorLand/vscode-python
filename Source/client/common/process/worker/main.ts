df6123b9684dcc746fbe78bbba189865722e58c5
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-18 05:20:33 +0200

    squash!

--- Source/client/common/process/worker/main.ts
+++ Source/client/common/process/worker/main.ts
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { Worker } from "worker_threads";
-import * as path from "path";
-import { traceVerbose, traceError } from "../../../logging/index";
+import { Worker } from 'worker_threads';
+import * as path from 'path';
+import { traceVerbose, traceError } from '../../../logging/index';
 
 /**
  * Executes a worker file. Make sure to declare the worker file as a entry in the webpack config.
@@ -12,42 +12,31 @@ import { traceVerbose, traceError } from "../../../logging/index";
  * @returns
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
-export async function executeWorkerFile(
-	workerFileName: string,
-	workerData: any
-): Promise<any> {
-	if (!workerFileName.endsWith(".worker.js")) {
-		throw new Error(
-			'Worker file must end with ".worker.js" for webpack to bundle webworkers'
-		);
-	}
-	return new Promise((resolve, reject) => {
-		const worker = new Worker(workerFileName, { workerData });
-		const id = worker.threadId;
-		traceVerbose(
-			`Worker id ${id} for file ${path.basename(
-				workerFileName
-			)} with data ${JSON.stringify(workerData)}`
-		);
-		worker.on("message", (msg: { err: Error; res: unknown }) => {
-			if (msg.err) {
-				reject(msg.err);
-			}
-			resolve(msg.res);
-		});
-		worker.on("error", (ex: Error) => {
-			traceError(`Error in worker ${workerFileName}`, ex);
-			reject(ex);
-		});
-		worker.on("exit", (code) => {
-			traceVerbose(`Worker id ${id} exited with code ${code}`);
-			if (code !== 0) {
-				reject(
-					new Error(
-						`Worker ${workerFileName} stopped with exit code ${code}`
-					)
-				);
-			}
-		});
-	});
+export async function executeWorkerFile(workerFileName: string, workerData: any): Promise<any> {
+    if (!workerFileName.endsWith('.worker.js')) {
+        throw new Error('Worker file must end with ".worker.js" for webpack to bundle webworkers');
+    }
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(workerFileName, { workerData });
+        const id = worker.threadId;
+        traceVerbose(
+            `Worker id ${id} for file ${path.basename(workerFileName)} with data ${JSON.stringify(workerData)}`,
+        );
+        worker.on('message', (msg: { err: Error; res: unknown }) => {
+            if (msg.err) {
+                reject(msg.err);
+            }
+            resolve(msg.res);
+        });
+        worker.on('error', (ex: Error) => {
+            traceError(`Error in worker ${workerFileName}`, ex);
+            reject(ex);
+        });
+        worker.on('exit', (code) => {
+            traceVerbose(`Worker id ${id} exited with code ${code}`);
+            if (code !== 0) {
+                reject(new Error(`Worker ${workerFileName} stopped with exit code ${code}`));
+            }
+        });
+    });
 }
--- Source/client/common/vscodeApis/windowApis.ts
+++ Source/client/common/vscodeApis/windowApis.ts
@@ -4,276 +4,232 @@
 /* eslint-disable max-classes-per-file */
 
 import {
-	CancellationToken,
-	MessageItem,
-	MessageOptions,
-	Progress,
-	ProgressOptions,
-	QuickPick,
-	QuickInputButtons,
-	QuickPickItem,
-	QuickPickOptions,
-	TextEditor,
-	window,
-	Disposable,
-	QuickPickItemButtonEvent,
-	Uri,
-} from "vscode";
-import { createDeferred, Deferred } from "../utils/async";
+    CancellationToken,
+    MessageItem,
+    MessageOptions,
+    Progress,
+    ProgressOptions,
+    QuickPick,
+    QuickInputButtons,
+    QuickPickItem,
+    QuickPickOptions,
+    TextEditor,
+    window,
+    Disposable,
+    QuickPickItemButtonEvent,
+    Uri,
+} from 'vscode';
+import { createDeferred, Deferred } from '../utils/async';
 
 export function showTextDocument(uri: Uri): Thenable<TextEditor> {
-	return window.showTextDocument(uri);
+    return window.showTextDocument(uri);
 }
 
 export function showQuickPick<T extends QuickPickItem>(
-	items: readonly T[] | Thenable<readonly T[]>,
-	options?: QuickPickOptions,
-	token?: CancellationToken
+    items: readonly T[] | Thenable<readonly T[]>,
+    options?: QuickPickOptions,
+    token?: CancellationToken,
 ): Thenable<T | undefined> {
-	return window.showQuickPick(items, options, token);
+    return window.showQuickPick(items, options, token);
 }
 
 export function createQuickPick<T extends QuickPickItem>(): QuickPick<T> {
-	return window.createQuickPick<T>();
+    return window.createQuickPick<T>();
 }
 
+export function showErrorMessage<T extends string>(message: string, ...items: T[]): Thenable<T | undefined>;
 export function showErrorMessage<T extends string>(
-	message: string,
-	...items: T[]
+    message: string,
+    options: MessageOptions,
+    ...items: T[]
 ): Thenable<T | undefined>;
-export function showErrorMessage<T extends string>(
-	message: string,
-	options: MessageOptions,
-	...items: T[]
-): Thenable<T | undefined>;
+export function showErrorMessage<T extends MessageItem>(message: string, ...items: T[]): Thenable<T | undefined>;
 export function showErrorMessage<T extends MessageItem>(
-	message: string,
-	...items: T[]
-): Thenable<T | undefined>;
-export function showErrorMessage<T extends MessageItem>(
-	message: string,
-	options: MessageOptions,
-	...items: T[]
+    message: string,
+    options: MessageOptions,
+    ...items: T[]
 ): Thenable<T | undefined>;
 
-export function showErrorMessage<T>(
-	message: string,
-	...items: any[]
-): Thenable<T | undefined> {
-	return window.showErrorMessage(message, ...items);
+export function showErrorMessage<T>(message: string, ...items: any[]): Thenable<T | undefined> {
+    return window.showErrorMessage(message, ...items);
 }
 
+export function showWarningMessage<T extends string>(message: string, ...items: T[]): Thenable<T | undefined>;
 export function showWarningMessage<T extends string>(
-	message: string,
-	...items: T[]
+    message: string,
+    options: MessageOptions,
+    ...items: T[]
 ): Thenable<T | undefined>;
-export function showWarningMessage<T extends string>(
-	message: string,
-	options: MessageOptions,
-	...items: T[]
-): Thenable<T | undefined>;
+export function showWarningMessage<T extends MessageItem>(message: string, ...items: T[]): Thenable<T | undefined>;
 export function showWarningMessage<T extends MessageItem>(
-	message: string,
-	...items: T[]
-): Thenable<T | undefined>;
-export function showWarningMessage<T extends MessageItem>(
-	message: string,
-	options: MessageOptions,
-	...items: T[]
+    message: string,
+    options: MessageOptions,
+    ...items: T[]
 ): Thenable<T | undefined>;
 
-export function showWarningMessage<T>(
-	message: string,
-	...items: any[]
-): Thenable<T | undefined> {
-	return window.showWarningMessage(message, ...items);
+export function showWarningMessage<T>(message: string, ...items: any[]): Thenable<T | undefined> {
+    return window.showWarningMessage(message, ...items);
 }
 
+export function showInformationMessage<T extends string>(message: string, ...items: T[]): Thenable<T | undefined>;
 export function showInformationMessage<T extends string>(
-	message: string,
-	...items: T[]
+    message: string,
+    options: MessageOptions,
+    ...items: T[]
 ): Thenable<T | undefined>;
-export function showInformationMessage<T extends string>(
-	message: string,
-	options: MessageOptions,
-	...items: T[]
-): Thenable<T | undefined>;
+export function showInformationMessage<T extends MessageItem>(message: string, ...items: T[]): Thenable<T | undefined>;
 export function showInformationMessage<T extends MessageItem>(
-	message: string,
-	...items: T[]
-): Thenable<T | undefined>;
-export function showInformationMessage<T extends MessageItem>(
-	message: string,
-	options: MessageOptions,
-	...items: T[]
+    message: string,
+    options: MessageOptions,
+    ...items: T[]
 ): Thenable<T | undefined>;
 
-export function showInformationMessage<T>(
-	message: string,
-	...items: any[]
-): Thenable<T | undefined> {
-	return window.showInformationMessage(message, ...items);
+export function showInformationMessage<T>(message: string, ...items: any[]): Thenable<T | undefined> {
+    return window.showInformationMessage(message, ...items);
 }
 
 export function withProgress<R>(
-	options: ProgressOptions,
-	task: (
-		progress: Progress<{ message?: string; increment?: number }>,
-		token: CancellationToken
-	) => Thenable<R>
+    options: ProgressOptions,
+    task: (progress: Progress<{ message?: string; increment?: number }>, token: CancellationToken) => Thenable<R>,
 ): Thenable<R> {
-	return window.withProgress(options, task);
+    return window.withProgress(options, task);
 }
 
 export function getActiveTextEditor(): TextEditor | undefined {
-	const { activeTextEditor } = window;
-	return activeTextEditor;
+    const { activeTextEditor } = window;
+    return activeTextEditor;
 }
 
-export function onDidChangeActiveTextEditor(
-	handler: (e: TextEditor | undefined) => void
-): Disposable {
-	return window.onDidChangeActiveTextEditor(handler);
+export function onDidChangeActiveTextEditor(handler: (e: TextEditor | undefined) => void): Disposable {
+    return window.onDidChangeActiveTextEditor(handler);
 }
 
 export enum MultiStepAction {
-	Back = "Back",
-	Cancel = "Cancel",
-	Continue = "Continue",
+    Back = 'Back',
+    Cancel = 'Cancel',
+    Continue = 'Continue',
 }
 
 export async function showQuickPickWithBack<T extends QuickPickItem>(
-	items: readonly T[],
-	options?: QuickPickOptions,
-	token?: CancellationToken,
-	itemButtonHandler?: (e: QuickPickItemButtonEvent<T>) => void
+    items: readonly T[],
+    options?: QuickPickOptions,
+    token?: CancellationToken,
+    itemButtonHandler?: (e: QuickPickItemButtonEvent<T>) => void,
 ): Promise<T | T[] | undefined> {
-	const quickPick: QuickPick<T> = window.createQuickPick<T>();
-	const disposables: Disposable[] = [quickPick];
+    const quickPick: QuickPick<T> = window.createQuickPick<T>();
+    const disposables: Disposable[] = [quickPick];
 
-	quickPick.items = items;
-	quickPick.buttons = [QuickInputButtons.Back];
-	quickPick.canSelectMany = options?.canPickMany ?? false;
-	quickPick.ignoreFocusOut = options?.ignoreFocusOut ?? false;
-	quickPick.matchOnDescription = options?.matchOnDescription ?? false;
-	quickPick.matchOnDetail = options?.matchOnDetail ?? false;
-	quickPick.placeholder = options?.placeHolder;
-	quickPick.title = options?.title;
+    quickPick.items = items;
+    quickPick.buttons = [QuickInputButtons.Back];
+    quickPick.canSelectMany = options?.canPickMany ?? false;
+    quickPick.ignoreFocusOut = options?.ignoreFocusOut ?? false;
+    quickPick.matchOnDescription = options?.matchOnDescription ?? false;
+    quickPick.matchOnDetail = options?.matchOnDetail ?? false;
+    quickPick.placeholder = options?.placeHolder;
+    quickPick.title = options?.title;
 
-	const deferred = createDeferred<T | T[] | undefined>();
+    const deferred = createDeferred<T | T[] | undefined>();
 
-	disposables.push(
-		quickPick,
-		quickPick.onDidTriggerButton((item) => {
-			if (item === QuickInputButtons.Back) {
-				deferred.reject(MultiStepAction.Back);
-				quickPick.hide();
-			}
-		}),
-		quickPick.onDidAccept(() => {
-			if (!deferred.completed) {
-				if (quickPick.canSelectMany) {
-					deferred.resolve(
-						quickPick.selectedItems.map((item) => item)
-					);
-				} else {
-					deferred.resolve(quickPick.selectedItems[0]);
-				}
+    disposables.push(
+        quickPick,
+        quickPick.onDidTriggerButton((item) => {
+            if (item === QuickInputButtons.Back) {
+                deferred.reject(MultiStepAction.Back);
+                quickPick.hide();
+            }
+        }),
+        quickPick.onDidAccept(() => {
+            if (!deferred.completed) {
+                if (quickPick.canSelectMany) {
+                    deferred.resolve(quickPick.selectedItems.map((item) => item));
+                } else {
+                    deferred.resolve(quickPick.selectedItems[0]);
+                }
 
-				quickPick.hide();
-			}
-		}),
-		quickPick.onDidHide(() => {
-			if (!deferred.completed) {
-				deferred.resolve(undefined);
-			}
-		}),
-		quickPick.onDidTriggerItemButton((e) => {
-			if (itemButtonHandler) {
-				itemButtonHandler(e);
-			}
-		})
-	);
-	if (token) {
-		disposables.push(
-			token.onCancellationRequested(() => {
-				quickPick.hide();
-			})
-		);
-	}
-	quickPick.show();
+                quickPick.hide();
+            }
+        }),
+        quickPick.onDidHide(() => {
+            if (!deferred.completed) {
+                deferred.resolve(undefined);
+            }
+        }),
+        quickPick.onDidTriggerItemButton((e) => {
+            if (itemButtonHandler) {
+                itemButtonHandler(e);
+            }
+        }),
+    );
+    if (token) {
+        disposables.push(
+            token.onCancellationRequested(() => {
+                quickPick.hide();
+            }),
+        );
+    }
+    quickPick.show();
 
-	try {
-		return await deferred.promise;
-	} finally {
-		disposables.forEach((d) => d.dispose());
-	}
+    try {
+        return await deferred.promise;
+    } finally {
+        disposables.forEach((d) => d.dispose());
+    }
 }
 
 export class MultiStepNode {
-	constructor(
-		public previous: MultiStepNode | undefined,
-		public readonly current: (
-			context?: MultiStepAction
-		) => Promise<MultiStepAction>,
-		public next: MultiStepNode | undefined
-	) {}
+    constructor(
+        public previous: MultiStepNode | undefined,
+        public readonly current: (context?: MultiStepAction) => Promise<MultiStepAction>,
+        public next: MultiStepNode | undefined,
+    ) {}
 
-	public static async run(
-		step: MultiStepNode,
-		context?: MultiStepAction
-	): Promise<MultiStepAction> {
-		let nextStep: MultiStepNode | undefined = step;
-		let flowAction = await nextStep.current(context);
-		while (nextStep !== undefined) {
-			if (flowAction === MultiStepAction.Cancel) {
-				return flowAction;
-			}
-			if (flowAction === MultiStepAction.Back) {
-				nextStep = nextStep?.previous;
-			}
-			if (flowAction === MultiStepAction.Continue) {
-				nextStep = nextStep?.next;
-			}
+    public static async run(step: MultiStepNode, context?: MultiStepAction): Promise<MultiStepAction> {
+        let nextStep: MultiStepNode | undefined = step;
+        let flowAction = await nextStep.current(context);
+        while (nextStep !== undefined) {
+            if (flowAction === MultiStepAction.Cancel) {
+                return flowAction;
+            }
+            if (flowAction === MultiStepAction.Back) {
+                nextStep = nextStep?.previous;
+            }
+            if (flowAction === MultiStepAction.Continue) {
+                nextStep = nextStep?.next;
+            }
 
-			if (nextStep) {
-				flowAction = await nextStep?.current(flowAction);
-			}
-		}
+            if (nextStep) {
+                flowAction = await nextStep?.current(flowAction);
+            }
+        }
 
-		return flowAction;
-	}
+        return flowAction;
+    }
 }
 
-export function createStepBackEndNode<T>(
-	deferred?: Deferred<T>
-): MultiStepNode {
-	return new MultiStepNode(
-		undefined,
-		async () => {
-			if (deferred) {
-				// This is to ensure we don't leave behind any pending promises.
-				deferred.reject(MultiStepAction.Back);
-			}
-			return Promise.resolve(MultiStepAction.Back);
-		},
-		undefined
-	);
+export function createStepBackEndNode<T>(deferred?: Deferred<T>): MultiStepNode {
+    return new MultiStepNode(
+        undefined,
+        async () => {
+            if (deferred) {
+                // This is to ensure we don't leave behind any pending promises.
+                deferred.reject(MultiStepAction.Back);
+            }
+            return Promise.resolve(MultiStepAction.Back);
+        },
+        undefined,
+    );
 }
 
-export function createStepForwardEndNode<T>(
-	deferred?: Deferred<T>,
-	result?: T
-): MultiStepNode {
-	return new MultiStepNode(
-		undefined,
-		async () => {
-			if (deferred) {
-				// This is to ensure we don't leave behind any pending promises.
-				deferred.resolve(result);
-			}
-			return Promise.resolve(MultiStepAction.Back);
-		},
-		undefined
-	);
+export function createStepForwardEndNode<T>(deferred?: Deferred<T>, result?: T): MultiStepNode {
+    return new MultiStepNode(
+        undefined,
+        async () => {
+            if (deferred) {
+                // This is to ensure we don't leave behind any pending promises.
+                deferred.resolve(result);
+            }
+            return Promise.resolve(MultiStepAction.Back);
+        },
+        undefined,
+    );
 }
